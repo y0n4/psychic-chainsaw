a8cbbd393fb0ed33747a9c86b5b98bcdf86ffb4a
Yona Nagayama <04l.yona@gmail.com>
2021-09-17 09:51:25 -0700

    feat: initialized stencil web components

---
+++ stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'play-switch',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
